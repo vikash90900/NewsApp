e6bd12e5ec8e14c2f465d7b610f548ca7476315e
vikash90900 <vikashtanwar7279@ce.du.ac.in>
2026-09-09 13:42:46 +0530

    Fixed Middeleware typo

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use("/api/notifications", notificationRoutes);
 
 // Basic test route
 app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is working...");
 });
 
 module.exports = app;
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
@@ -15,3 +15,5 @@ module.exports = (req, res, next) => {
     res.status(401).json({ message: "Invalid token" });
   }
 };
+
+module.exports = authMiddleware;
