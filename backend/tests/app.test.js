4f93c7155a06303aa1a9f1269d523d182fce2ba1
vikash90900 <vikashtanwar7279@ce.du.ac.in>
2026-09-18 19:02:32 +0530

    Added Frontend CI and Docker image pipeline

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -9,3 +9,12 @@ describe("GET /", () => {
     expect(response.text).toBe("API is running...");
   });
 });
+
+describe("GET /health", () => {
+  test("should return status OK with 200 status code", async () => {
+    const response = await request(app).get("/health");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ status: "OK" });
+  });
+});
